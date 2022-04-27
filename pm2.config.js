b652e897a1037e1947f35f613d62872d423ae115
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2022-04-27 15:56:11 +0530

    feat: changed payload commit to use go service. Also added missing audit-protocol-backend process.

--- pm2.config.js
+++ pm2.config.js
@@ -3,22 +3,35 @@ const MAX_RESTART = 10;
 const MIN_UPTIME = 60000; 
 
 const NODE_ENV = "development" // process.env.NODE_ENV || 'development';
+const CWD = "/home/ubuntu/audit-protocol-private/"
 
 module.exports = {
   apps : [
     {
-      name   : "audit-protocol-payload-commit-parallel",
-      script : "python3 ./payload_commit_service_parallel.py",
+      name   : "audit-protocol-backend",
+      script : "python3 ./gunicorn_main_launcher.py",
+      cwd : CWD,
       max_restarts: MAX_RESTART,
       min_uptime: MIN_UPTIME,
       env: {
-        NODE_ENV: NODE_ENV,
-        PY_IPFS_HTTP_CLIENT_PREFER_HTTPX: "yes"
-      }
+        NODE_ENV: NODE_ENV
+      },
+    },
+    {
+      name   : "audit-protocol-payload-commit",
+      script : "./go-payload-commit-service/payloadCommitService",
+      cwd : CWD,
+      max_restarts: MAX_RESTART,
+      min_uptime: MIN_UPTIME,
+      env: {
+        NODE_ENV: NODE_ENV
+      },
+      args: "5" //Log level set to debug, for production change to 4 (INFO) or 2(ERROR)
     },
     {
       name   : "audit-protocol-webhooks",
       script : "python3 ./gunicorn_webhook_launcher.py",
+      cwd : CWD,
       max_restarts: MAX_RESTART,
       min_uptime: MIN_UPTIME,
       env: {
@@ -28,12 +41,13 @@ module.exports = {
     {
       name   : "audit-protocol-dag-verifier",
       script : "./dag_verifier/dagChainVerifier",
+      cwd : CWD,
       max_restarts: MAX_RESTART,
       min_uptime: MIN_UPTIME,
       env: {
         NODE_ENV: NODE_ENV,
       },
-      args: "5"
+      args: "5" //Log level set to debug, for production change to 4 (INFO) or 2(ERROR)
     }
   ]
 }
