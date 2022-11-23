ac058c57ec9259accb68e250b2936aa52c2fe8c0
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2022-11-23 22:04:40 +0530

    migrated token aggregator from pooler

--- update_namespace.sh
+++ update_namespace.sh
@@ -6,17 +6,21 @@ NAMESPACE="SUSHISWAP-stg-1"
 echo "Updating namespace to $NAMESPACE"
 
 sed -i "s/const NAMESPACE string = \"UNISWAPV2\"/const NAMESPACE string = \"$NAMESPACE\"/g" dag_verifier/dag_verifier.go
+sed -i "s/const NAMESPACE string = \"UNISWAPV2\"/const NAMESPACE string = \"$NAMESPACE\"/g" token-aggregator/main.go
 
 sed -i "s/NAMESPACE = 'UNISWAPV2'/NAMESPACE = '$NAMESPACE'/g" pair_data_aggregation_service.py utils/redis_keys.py
 
 sed -i "s/cache:indexesRequested/cache:indexesRequested:$NAMESPACE/g" proto_sliding_window_cacher_service.py
 
-sed -i "s/audit-protocol-proto-indexer/$NAMESPACE-audit-protocol-proto-indexer/g" pm2.config.js
-sed -i "s/audit-protocol-dag-verifier/$NAMESPACE-audit-protocol-dag-verifier/g" pm2.config.js
+sed -i "s/ap-proto-indexer/ap-proto-indexer-$NAMESPACE/g" pm2.config.js
+sed -i "s/ap-dag-verifier/ap-dag-verifier-$NAMESPACE/g" pm2.config.js
 
 sed -i "s/NAMESPACE = \"UNISWAPV2\"/NAMESPACE = \"$NAMESPACE\"/g" register_pair_projects_for_indexing.py
 
 echo "Building dag_verifier after changes"
 cd dag_verifier
+go build .
+echo "Building token-aggregator after changes"
+cd ../token-aggregator
 go build .
 cd ..
