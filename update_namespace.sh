d620408478087375ad5469ebf270ee08a967aa29
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2023-01-10 13:08:55 +0530

    fix: uncomment namespace change for indexes key, commented by mistake for testing

--- update_namespace.sh
+++ update_namespace.sh
@@ -9,7 +9,7 @@ sed -i "s/const NAMESPACE string = \"UNISWAPV2\"/const NAMESPACE string = \"$NAM
 
 sed -i "s/NAMESPACE = 'UNISWAPV2'/NAMESPACE = '$NAMESPACE'/g" pair_data_aggregation_service.py utils/redis_keys.py
 
-#sed -i "s/cache:indexesRequested/cache:indexesRequested:$NAMESPACE/g" proto_sliding_window_cacher_service.py
+sed -i "s/cache:indexesRequested/cache:indexesRequested:$NAMESPACE/g" proto_sliding_window_cacher_service.py
 
 namespace=$(echo $NAMESPACE | tr '[:upper:]' '[:lower:]')
 
