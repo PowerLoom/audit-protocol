b741386a7bd76abff5444b7fe457d8497e78b750
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2023-01-12 11:48:44 +0530

    chore:update namespace if not already updated

--- update_namespace.sh
+++ update_namespace.sh
@@ -3,18 +3,20 @@
 NAMESPACE="UNISWAPV2-ph15-prod"
 
 # Now take action
-echo "Updating namespace to $NAMESPACE"
+
+grep $NAMESPACE token-aggregator/main.go
+if [ $? -eq 0 ]; then
+    echo "Namespace $NAMESPACE" is already updated
+    exit 0
+else
+    echo "Updating namespace to $NAMESPACE"
+fi
 
 sed -i "s/const NAMESPACE string = \"UNISWAPV2\"/const NAMESPACE string = \"$NAMESPACE\"/g" token-aggregator/main.go
 
 sed -i "s/NAMESPACE = 'UNISWAPV2'/NAMESPACE = '$NAMESPACE'/g" pair_data_aggregation_service.py utils/redis_keys.py
 
 sed -i "s/cache:indexesRequested/cache:indexesRequested:$NAMESPACE/g" proto_sliding_window_cacher_service.py
-
-namespace=$(echo $NAMESPACE | tr '[:upper:]' '[:lower:]')
-
-sed -i "s/NAMESPACE = \"UNISWAPV2\"/NAMESPACE = \"$NAMESPACE\"/g" register_pair_projects_for_indexing.py
-
 
 echo "Building token-aggregator after changes"
 cd token-aggregator
